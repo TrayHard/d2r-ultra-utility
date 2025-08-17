efe0a1932310a3a13fa20dc5f9622368f0440afc
Poliakov Ilia <i.polyakov95@yandex.ru>
2025-08-17 04:49:50 +0300

    build fix

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -260,6 +260,11 @@ fn is_system_directory(dir_name: &str) -> bool {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(desktop)]
+            app.handle().plugin(tauri_plugin_updater::Builder::new().build())?;
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_fs::init())
         .invoke_handler(tauri::generate_handler![
